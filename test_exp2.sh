5c30370439f537937e0a1e05ee8f409428bdf238
Daniel Sam Pete Thiyagu <danielsamfdo@gmail.com>
2017-04-26 14:54:58 -0400

    Adding Tests and Experiments to Run

---
+++ test_exp2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=testExp2
+#SBATCH --output=testExp2_output.txt  # output file
+#SBATCH -e testExp2_error.err        # File to which STDERR will be written 
+#SBATCH --partition=m40-long
+#SBATCH --ntasks=2
+#SBATCH --ntasks-per-node=8
+#SBATCH --nodes=1
+#SBATCH --time=1-4:00:00         # Runtime in D-HH:MM
+#SBATCH --mem-per-cpu=MaxMemPerCPU    # Memory in MB per cpu allocated
+pip install -r requirement.txt --user
+python testing_other_experiments_2.py
